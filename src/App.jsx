325d9237cf9a71cd212bd42af41ac07b19e75bcd
Will Macaulay <58277689+will-macaulay@users.noreply.github.com>
2025-11-19 19:12:11 -0600

    Attempt 3 at fixing deployed app issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import { HashRouter, Route, Routes } from 'react-router'
 import { useState } from 'react'
-import { Route, Routes, HashRouter } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
 import AboutMe from './components/AboutMe'
